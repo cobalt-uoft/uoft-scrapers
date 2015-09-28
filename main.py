78f1d5816c209046a6e0a128e9e19f5ebb615ce4
Qasim Iqbal <me@qas.im>
2015-09-28 00:26:37 -0400

    New spacing config

--- main.py
+++ main.py
@@ -10,59 +10,59 @@ from scrapers.map.main import Map
 
 
 class UofTScrapers:
-  """Static class wrapping all the scrapers."""
+    """Static class wrapping all the scrapers."""
 
-  def scrape_all():
-      scrape_cf()
-      scrape_utmt()
-      scrape_utsct()
-      scrape_utsgt()
-      scrape_utsgc()
-      scrape_m()
+    def scrape_all():
+        scrape_cf()
+        scrape_utmt()
+        scrape_utsct()
+        scrape_utsgt()
+        scrape_utsgc()
+        scrape_m()
 
-  def scrape_cf():
-      cf = Coursefinder()
-      cf.update_files()
+    def scrape_cf():
+        cf = Coursefinder()
+        cf.update_files()
 
-  def scrape_utmt():
-      utmt = UTMTimetable()
-      utmt.update_files()
+    def scrape_utmt():
+        utmt = UTMTimetable()
+        utmt.update_files()
 
-  def scrape_utsct():
-      utsct = UTSCTimetable()
-      utsct.update_files()
+    def scrape_utsct():
+        utsct = UTSCTimetable()
+        utsct.update_files()
 
-  def scrape_utsgt():
-      utsgt = UTSGTimetable()
-      utsgt.update_files()
+    def scrape_utsgt():
+        utsgt = UTSGTimetable()
+        utsgt.update_files()
 
-  def scrape_utsgc():
-      utsgc = UTSGCalendar()
-      utsgc.update_files()
+    def scrape_utsgc():
+        utsgc = UTSGCalendar()
+        utsgc.update_files()
 
-  def scrape_m():
-      m = Map()
-      m.update_files()
+    def scrape_m():
+        m = Map()
+        m.update_files()
 
 
 if __name__ == "__main__":
-  args = [x.lower() for x in sys.argv]
+    args = [x.lower() for x in sys.argv]
 
-  if 'all' in args:
-      UofTScrapers.scrape_all()
-  else:
-      for x in args:
-          if x == 'coursefinder':
-              UofTScrapers.scrape_cf()
-          elif x == 'utmtimetable':
-              UofTScrapers.scrape_utmt()
-          elif x == 'utsctimetable':
-              UofTScrapers.scrape_utsct()
-          elif x == 'utsgtimetable':
-              UofTScrapers.scrape_utsgt()
-          elif x == 'utsgcalendar':
-              UofTScrapers.scrape_utsgc()
-          elif x == 'map':
-              UofTScrapers.scrape_m()
+    if 'all' in args:
+        UofTScrapers.scrape_all()
+    else:
+        for x in args:
+            if x == 'coursefinder':
+                UofTScrapers.scrape_cf()
+            elif x == 'utmtimetable':
+                UofTScrapers.scrape_utmt()
+            elif x == 'utsctimetable':
+                UofTScrapers.scrape_utsct()
+            elif x == 'utsgtimetable':
+                UofTScrapers.scrape_utsgt()
+            elif x == 'utsgcalendar':
+                UofTScrapers.scrape_utsgc()
+            elif x == 'map':
+                UofTScrapers.scrape_m()
 
-  print("Finished.", flush=True)
+    print("Finished.", flush=True)
