f1da881fc1dc3fd7749fa9ede7e89c6689ee5e89
Qasim Iqbal <me@qas.im>
2016-03-24 02:01:31 -0400

    Add polygons and more accurate latitude/longitude scraping

--- uoftscrapers/__init__.py
+++ uoftscrapers/__init__.py
@@ -7,7 +7,7 @@ from .scrapers.utmtimetable import UTMTimetable
 from .scrapers.utsctimetable import UTSCTimetable
 from .scrapers.utsgtimetable import UTSGTimetable
 from .scrapers.utsgcalendar import UTSGCalendar
-from .scrapers.map import Map
+from .scrapers.buildings import Buildings
 
 class NullHandler(logging.Handler):
     def emit(self, record):
--- uoftscrapers/scrapers/buildings/__init__.py
+++ uoftscrapers/scrapers/buildings/__init__.py
@@ -4,17 +4,18 @@ from collections import OrderedDict
 import os
 import re
 import json
+from decimal import *
 from ..scraper import Scraper
 
 
-class Map(Scraper):
-    """A scraper for UofT's Map web service.
+class Buildings(Scraper):
+    """A scraper for UofT's buildings.
 
     UofT Map is located at http://map.utoronto.ca/.
     """
 
     def __init__(self, output_location='.'):
-        super().__init__('Map', output_location)
+        super().__init__('Buildings', output_location)
 
         self.host = 'http://map.utoronto.ca/'
         self.campuses = ['utsg', 'utm', 'utsc']
@@ -25,6 +26,8 @@ class Map(Scraper):
 
         for campus in self.campuses:
             data = self.get_map_json(campus)
+            regions = self.get_regions_json(campus)['buildings']
+
             for building in data['buildings']:
                 _id = building['id']
                 code = building['code']
@@ -39,21 +42,33 @@ class Map(Scraper):
                 country = self.get_value(building, 'country')
                 postal = self.get_value(building, 'postal')
 
+                polygon = []
+                for region in regions:
+                    if region['id'] == _id:
+                        lat_lng = region['center_point']
+                        if lat_lng:
+                            lat_lng = lat_lng[1:-2].split(', ')
+                            if len(lat_lng) == 2:
+                                lat = float(lat_lng[0])
+                                lng = float(lat_lng[1])
+                            polygon = region['points']
+
                 doc = OrderedDict([
                     ('id', _id),
                     ('code', code),
                     ('name', name),
                     ('short_name', short_name),
                     ('campus', campus.upper()),
-                    ('lat', lat),
-                    ('lng', lng),
                     ('address', OrderedDict([
                         ('street', street),
                         ('city', city),
                         ('province', province),
                         ('country', country),
                         ('postal', postal)
-                    ]))
+                    ])),
+                    ('lat', lat),
+                    ('lng', lng),
+                    ('polygon', polygon)
                 ])
 
                 with open('%s/%s.json' % (self.location, _id), 'w') as fp:
@@ -71,6 +86,18 @@ class Map(Scraper):
             'Referer': self.host
         }
         html = self.s.get('%s%s%s' % (self.host, 'data/map/', campus),
+                          headers=headers).text
+        data = json.loads(html)
+        return data
+
+    def get_regions_json(self, campus):
+        """Retrieve the JSON structure from host."""
+
+        self.s.get(self.host)
+        headers = {
+            'Referer': self.host
+        }
+        html = self.s.get('%s%s%s' % (self.host, 'data/regions/', campus),
                           headers=headers).text
         data = json.loads(html)
         return data
