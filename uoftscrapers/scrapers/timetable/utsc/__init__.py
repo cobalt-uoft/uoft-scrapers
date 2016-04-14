077944aa66375333c95f89ab001baa5c4652feeb
Eugene Cheung <ey.cheung@outlook.com>
2016-04-13 20:21:44 -0400

    Move calendar and timetable scrapers into subdirectories

--- uoftscrapers/scrapers/timetable/utsc/__init__.py
+++ uoftscrapers/scrapers/timetable/utsc/__init__.py
@@ -1,17 +1,17 @@
-from ..scraper import Scraper
+from ...scraper import Scraper
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 import json
 import os
 import requests
 
-class UTMTimetable:
+class UTSCTimetable:
 
-    host = 'https://student.utm.utoronto.ca/timetable/'
+    host = 'http://www.utsc.utoronto.ca/~registrar/scheduling/timetable'
 
     @staticmethod
     def scrape(location='.'):
-        Scraper.logger.info('UTMTimetable initialized.')
+        Scraper.logger.info('UTSCTimetable initialized.')
         Scraper.logger.info('Not implemented.')
         raise NotImplementedError('This scraper has not been implemented yet.')
-        Scraper.logger.info('UTMTimetable completed.')
+        Scraper.logger.info('UTSCTimetable completed.')
