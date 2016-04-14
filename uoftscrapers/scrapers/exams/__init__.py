31cd6821fe0cc3dea3950b38a4115d98c9557d38
Kashav Madan <kshvmdn@gmail.com>
2016-04-13 21:19:49 -0400

    Unique id, course_id + ISO dates

--- uoftscrapers/scrapers/exams/__init__.py
+++ uoftscrapers/scrapers/exams/__init__.py
@@ -4,6 +4,7 @@ import json
 import requests
 from datetime import datetime, date
 from ..scraper import Scraper
+import pytz
 
 
 class Exams:
@@ -22,84 +23,129 @@ class Exams:
         Scraper.logger.info('Exams initialized.')
         Scraper.ensure_location(location)
 
-        all_exams = OrderedDict()
+        exams = OrderedDict()
 
-        for m in Exams.get_exam_months(year):
+        for p in Exams.get_exam_periods(year):
 
-            exams = OrderedDict()
-
-            Scraper.logger.info('Scraping %s exams.' % m.upper())
+            Scraper.logger.info('Scraping %s exams.' % p.upper())
 
             headers = {
                 'Referer': Exams.host
             }
-            html = Exams.s.get('%s%s' % (Exams.host, m), headers=headers).text
+            html = Exams.s.get('%s%s' % (Exams.host, p), headers=headers).text
             soup = BeautifulSoup(html, 'html.parser')
 
             if not soup.find('table', class_='vertical listing'):
                 # no exam data available
+                Scraper.logger.info('No exams for %s.' % p.upper())
                 continue
 
             rows = soup.find('table', class_='vertical listing').find_all('tr')
             for row in rows[1:]:
                 data = [x.text.strip() for x in row.find_all('td')]
 
-                course, section, location_ = data[0], data[1], data[4]
-                date_ = Exams.parse_date(data[2], m[-2:]) or ''
-                start, end = Exams.parse_time(data[3]) or (0, 0)
+                id_, course_id, course_code = Exams.parse_course_info(p, data[0])
 
-                if course not in exams:
-                    exams[course] = OrderedDict([
-                        ('date', date_),
-                        ('start_time', start),
-                        ('end_time', end),
-                        ('sections', [])
-                    ])
+                if id_ is None:
+                    continue
 
-                exams[course]['sections'].append(OrderedDict([
+                section, location_ = data[1], data[4]
+                date_ = Exams.parse_date(data[2], p[-2:]) or ''
+                start, end = Exams.parse_time(data[3], date_) or (0, 0)
+
+                doc = OrderedDict([
+                    ('id', id_),
+                    ('course_id', course_id),
+                    ('course_code', course_code),
+                    ('period', p),
+                    ('date', date_),
+                    ('start_time', start),
+                    ('end_time', end),
+                    ('sections', [])
+                ])
+
+                if id_ not in exams:
+                    exams[id_] = doc
+
+                exams[id_]['sections'].append(OrderedDict([
                     ('section', section),
                     ('location', location_)
                 ]))
 
-            all_exams[m] = exams
-
-        for m in all_exams:
-            with open('%s/%s.json' % (location, m.upper()), 'w+') as outfile:
-                json.dump(all_exams[m], outfile)
+        for id_, doc in exams.items():
+            with open('%s/%s.json' % (location, id_), 'w+') as outfile:
+                json.dump(doc, outfile)
 
         Scraper.logger.info('Exams completed.')
+
+    @staticmethod
+    def parse_course_info(period, course_code):
+
+        month, year, season = period[:-2], period[-2:], course_code[-1]
+
+        endings = {
+            'dec': {
+                'F': '%s9' % str(int(year) - 1),
+                'Y': '%s9' % str(int(year) - 1)
+            },
+            'apr': {
+                'S': '%s1' % str(year),
+                'Y': '%s9' % str(int(year) - 1)
+            },
+            'june': {
+                'F': '%s5F' % str(year),
+                'Y': '%s5' % str(year)
+            },
+            'aug': {
+                'S': '%s5S' % str(year),
+                'Y': '%s5' % str(year)
+            }
+        }
+
+        try:
+            course_id = '%s20%s' % (course_code, endings[month][season])
+        except KeyError:
+            return None, None, None
+
+        exam_id = '%s%s' % (course_id, period.upper())
+        return exam_id, course_id, course_code
 
     @staticmethod
     def parse_date(date_, year):
         """Convert date of form `D DD MMM` to ISO 8601 format."""
 
         date_ = date_.split(' ')
-        if len(date_) >= 3:
+        if len(date_) == 3:
             day, date_, month = date_
 
             # TODO add EST offset
             return datetime.strptime('%s %s %s %s' % (day, date_, month, year),
-                                     '%a %d %b %y').isoformat()
+                                     '%a %d %b %y').date().isoformat()
 
     @staticmethod
-    def parse_time(time):
+    def parse_time(time, date_):
         """Convert time range of form `pd hh:mm - hh:mm` to start and end
         decimal hours."""
 
-        def convert_time(t, is_pm=False):
+        def convert_time_to_iso(t, is_pm=False):
             h, m = [int(x) for x in t.split(':')]
             h += 12 if is_pm else 0
-            return h + (m / 60)
+            m //= 60
+
+            return datetime.strptime('%s %s %s' % (date_, h, m),
+                                     '%Y-%m-%d %H %M').replace(
+                tzinfo=pytz.timezone('US/Eastern')).isoformat()
 
         time = list(filter(None, time.replace('-', '').split(' ')))
-        if len(time) >= 3:
+        if len(time) == 3:
             period, start, end = time
-
             after_12 = period == 'PM' or period == 'EV'
-            return convert_time(start, after_12), convert_time(end, after_12)
+
+            return convert_time_to_iso(start, after_12), \
+                   convert_time_to_iso(end, after_12)
 
     @staticmethod
-    def get_exam_months(year):
+    def get_exam_periods(year):
         if not year:
             year = date.today().year
 
