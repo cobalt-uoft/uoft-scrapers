1aa6237a5327369446eeab791446b174755db467
Eugene Cheung <ey.cheung@outlook.com>
2016-04-14 22:13:15 -0400

    Output correctly parsed UTM shuttle data

--- uoftscrapers/scrapers/utmshuttle/__init__.py
+++ uoftscrapers/scrapers/utmshuttle/__init__.py
@@ -1,12 +1,13 @@
 from ..scraper import Scraper
 from bs4 import BeautifulSoup
-import http.cookiejar
+import datetime
 import json
 import logging
 import os
 import re
 import requests
 import sys
+import time
 
 class UTMShuttle:
     """A scraper for the UTM shuttle bus schedule.
@@ -14,22 +15,30 @@ class UTMShuttle:
     The schedule is located at https://m.utm.utoronto.ca/shuttle.php.
     """
 
-    host = 'https://m.utm.utoronto.ca/shuttleByDate.php?year=%s&month=%s&day=%s'
+    host = "https://m.utm.utoronto.ca/shuttleByDate.php?year=%s&month=%s&day=%s"
     logger = logging.getLogger("uoftscrapers")
-    cookies = http.cookiejar.CookieJar()
     s = requests.Session()
-    threads = 32
 
     @staticmethod
-    def scrape(location='.'):
+    def scrape(location=".", month=None):
         """Update the local JSON files for this scraper."""
 
-        Scraper.logger.info('UTMShuttle initialized.')
+        Scraper.logger.info("UTMShuttle initialized.")
         Scraper.ensure_location(location)
 
-        html = UTMShuttle.get_schedule_html(UTMShuttle.host % ('2016', '04', '22'))
+        if month is None:
+            month = "04"  # TODO: get current month
 
-        UTMShuttle.parse_course_html(html)
+        # TODO: pad month if needed and get year
+
+        # TODO: do for all days in month
+        html = UTMShuttle.get_schedule_html(UTMShuttle.host % ("2016", "04", "22"))
+        json = UTMShuttle.parse_schedule_html(html)
+
+        # TODO: output to files
+        print(json)
+
+        Scraper.logger.info("UTMShuttle completed.")
 
     @staticmethod
     def get_schedule_html(url):
@@ -45,26 +54,61 @@ class UTMShuttle:
                     requests.exceptions.ConnectionError):
                 continue
 
-        return html.encode('utf-8')
+        return html.encode("utf-8")
 
     @staticmethod
-    def parse_course_html(html):
+    def parse_schedule_html(html):
         """Create JSON files from the HTML pages downloaded."""
 
         soup = BeautifulSoup(html, "html.parser")
 
-        # get options (routes): names and corresponding ul ids
+        # Get date
+        html_date = time.strptime(soup.find("h2").get_text().strip(), '%b %d %Y')
+        date = time.strftime('%Y-%m-%d', html_date)
+
+        # Get route data
         routes = {}
-        for route in soup.find(id="chooseRoute").find_all("option"):
-            route_id = route.get('value')
+        for html_route_opt in soup.find(id="chooseRoute").find_all("option"):
+            html_route_id = html_route_opt.get("value")
+            html_route_times = soup.find(id=html_route_id).find_all("li")
 
-            route_times = soup.find(id=route_id).find_all("li")
+            route_name = html_route_opt.get_text().strip().split(" @ ")
 
-            routes[route_id] = {
-                'name': route.get_text().strip(),
-                'times': [time.get_text().strip() for time in route_times]
+            times = []
+            for html_time in html_route_times:
+                html_time_text = html_time.get_text().strip().lower()
+
+                time_rush_hour = "rush hour" in html_time_text
+                time_no_overload = "no overload" in html_time_text
+
+                html_time_clean = re.sub(' \*.*\*| (a|p)m', '', html_time_text).strip()
+
+                times.append({
+                    "time": "%sT%s" % (
+                        date,
+                        time.strftime("%H:%M:00-04:00", time.strptime(html_time_clean, "%I:%M"))
+                    ),
+                    "rush_hour": time_rush_hour,
+                    "no_overload": time_no_overload
+                })
+
+            route_id = re.sub('\.|ROUTE| ', '', route_name[0].upper())
+            route_stops = {
+                "location": route_name[1],
+                "building_id": "123",  # TODO: get building_id
+                "times": times
             }
 
+            if route_id in routes.keys():
+                routes[route_id]["stops"].append(route_stops)
+            else:
+                routes[route_id] = {
+                    "id": route_id,
+                    "name": route_name[0],
+                    "stops": [route_stops]
+                }
 
-        print(routes)
-
+        return {
+            "date": date,
+            "routes": [v for k, v in routes.items()]
+        }
