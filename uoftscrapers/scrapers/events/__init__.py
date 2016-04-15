85adbffd94f8c94c26356d19666f60b8d070ba17
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-04-15 11:14:51 -0400

    Base file for the Events scraper

---
+++ uoftscrapers/scrapers/events/__init__.py
@@ -0,0 +1,18 @@
+from ...scraper import Scraper
+from datetime import datetime, date
+from collections import OrderedDict
+import json
+import requests
+
+class UTEvents:
+    """A scraper for Events at the University of Toronto."""
+
+    host = 'https://www.events.utoronto.ca/'
+    s = requests.Session()
+
+    @staticmethod
+    def scrape(location='.'):
+        Scraper.logger.info('UTEvents initialized.')
+        Scraper.logger.info('Not implemented.')
+        raise NotImplementedError('This scraper has not been implemented yet.')
+        Scraper.logger.info('UTEvents completed.')
