40f6e7c1c9ba53a9e0a71a071feb63711819866e
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-04-15 13:58:55 -0400

    Add Campuses references

--- uoftscrapers/scrapers/events/__init__.py
+++ uoftscrapers/scrapers/events/__init__.py
@@ -1,16 +1,23 @@
-from ...scraper import Scraper
+from bs4 import BeautifulSoup
 from datetime import datetime, date
 from collections import OrderedDict
-from bs4 import BeautifulSoup
-import pytz
-
-import json
+from ..scraper import Scraper
 import requests
+import pytz
+import json
 
 class UTEvents:
     """A scraper for Events at the University of Toronto."""
 
     host = 'https://www.events.utoronto.ca/'
+
+    campuses = [
+    	{"id" : 1, "name" : "St. George", "tag" : "utsg"},
+     	{"id" : 2, "name" : "U of T Mississauga", "tag" : "utm"},
+     	{"id" : 3, "name" : "U of T Scarborough", "tag" : "utsc"}, 
+     	{"id" : 0, "name" : "Off Campus", "tag" : "off"},
+     	]
+
     s = requests.Session()
 
     @staticmethod
