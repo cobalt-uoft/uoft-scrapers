531b8d3f6d155e6dfcb6165087c04a0bdbf5139b
Eugene Cheung <ey.cheung@outlook.com>
2016-04-14 22:47:59 -0400

    Fetch all UTM bus schedules for the month and write to JSON

--- uoftscrapers/scrapers/utmshuttle/__init__.py
+++ uoftscrapers/scrapers/utmshuttle/__init__.py
@@ -1,5 +1,7 @@
 from ..scraper import Scraper
 from bs4 import BeautifulSoup
+from calendar import monthrange
+from collections import OrderedDict
 import datetime
 import json
 import logging
@@ -19,24 +21,33 @@ class UTMShuttle:
     logger = logging.getLogger("uoftscrapers")
     s = requests.Session()
 
+    building_ids = {
+        "Instructional Centre Layby": "334",
+        "Hart House": "002",
+        "Deerfield Hall North Layby": "340"
+    }
+
     @staticmethod
-    def scrape(location=".", month=None):
+    def scrape(location=".", month=3):
         """Update the local JSON files for this scraper."""
 
         Scraper.logger.info("UTMShuttle initialized.")
         Scraper.ensure_location(location)
 
-        if month is None:
-            month = "04"  # TODO: get current month
+        now = datetime.datetime.now()
+        year = now.strftime("%Y")
+        month = now.strftime("%m") if month is None else str(month).zfill(2)
+        days = monthrange(int(year), int(month))[1]
 
-        # TODO: pad month if needed and get year
+        for day in range(days):
+            html = UTMShuttle.get_schedule_html(UTMShuttle.host % (year, month, day + 1))
+            schedule = UTMShuttle.parse_schedule_html(html)
 
-        # TODO: do for all days in month
-        html = UTMShuttle.get_schedule_html(UTMShuttle.host % ("2016", "04", "22"))
-        json = UTMShuttle.parse_schedule_html(html)
-
-        # TODO: output to files
-        print(json)
+            with open('%s/%s.json' % (
+                location,
+                "%s-%s-%s" % (year, month, "{0:02d}".format(day + 1))
+            ),'w+') as outfile:
+                json.dump(schedule, outfile)
 
         Scraper.logger.info("UTMShuttle completed.")
 
@@ -58,7 +69,7 @@ class UTMShuttle:
 
     @staticmethod
     def parse_schedule_html(html):
-        """Create JSON files from the HTML pages downloaded."""
+        """Create JSON object from the HTML page download."""
 
         soup = BeautifulSoup(html, "html.parser")
 
@@ -68,47 +79,53 @@ class UTMShuttle:
 
         # Get route data
         routes = {}
-        for html_route_opt in soup.find(id="chooseRoute").find_all("option"):
-            html_route_id = html_route_opt.get("value")
-            html_route_times = soup.find(id=html_route_id).find_all("li")
+        html_route_opts = soup.find(id="chooseRoute")
+        if html_route_opts:
+            for html_route_opt in html_route_opts.find_all("option"):
+                html_route_id = html_route_opt.get("value")
+                html_route_times = soup.find(id=html_route_id).find_all("li")
 
-            route_name = html_route_opt.get_text().strip().split(" @ ")
+                route_name = html_route_opt.get_text().strip().split(" @ ")
 
-            times = []
-            for html_time in html_route_times:
-                html_time_text = html_time.get_text().strip().lower()
+                times = []
+                for html_time in html_route_times:
+                    html_time_text = html_time.get_text().strip().lower()
 
-                time_rush_hour = "rush hour" in html_time_text
-                time_no_overload = "no overload" in html_time_text
+                    time_rush_hour = "rush hour" in html_time_text
+                    time_no_overload = "no overload" in html_time_text
 
-                html_time_clean = re.sub(' \*.*\*| (a|p)m', '', html_time_text).strip()
+                    html_time_clean = re.sub(' \*.*\*| (a|p)m', '', html_time_text).strip()
 
-                times.append({
-                    "time": "%sT%s" % (
-                        date,
-                        time.strftime("%H:%M:00-04:00", time.strptime(html_time_clean, "%I:%M"))
-                    ),
-                    "rush_hour": time_rush_hour,
-                    "no_overload": time_no_overload
-                })
+                    times.append(OrderedDict([
+                        ("time", "%sT%s" % (
+                            date,
+                            time.strftime("%H:%M:00-04:00", time.strptime(html_time_clean, "%I:%M"))
+                        )),
+                        ("rush_hour", time_rush_hour),
+                        ("no_overload", time_no_overload)
+                    ]))
 
-            route_id = re.sub('\.|ROUTE| ', '', route_name[0].upper())
-            route_stops = {
-                "location": route_name[1],
-                "building_id": "123",  # TODO: get building_id
-                "times": times
-            }
+                route_id = re.sub('\.|ROUTE| ', '', route_name[0].upper())
 
-            if route_id in routes.keys():
-                routes[route_id]["stops"].append(route_stops)
-            else:
-                routes[route_id] = {
-                    "id": route_id,
-                    "name": route_name[0],
-                    "stops": [route_stops]
-                }
+                # TODO: fetch this dynamically
+                route_building_id = UTMShuttle.building_ids[route_name[1]] if route_name[1] in UTMShuttle.building_ids else ""
 
-        return {
-            "date": date,
-            "routes": [v for k, v in routes.items()]
-        }
+                route_stops = OrderedDict([
+                    ("location", route_name[1]),
+                    ("building_id", route_building_id),
+                    ("times", times)
+                ])
+
+                if route_id in routes.keys():
+                    routes[route_id]["stops"].append(route_stops)
+                else:
+                    routes[route_id] = OrderedDict([
+                        ("id", route_id),
+                        ("name", route_name[0]),
+                        ("stops", [route_stops])
+                    ])
+
+        return OrderedDict([
+            ("date", date),
+            ("routes", [v for k, v in routes.items()])
+        ])
