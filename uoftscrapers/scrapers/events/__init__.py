d4b0235395e974ec9fc960e6dbe458ac32f4e686
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-04-19 11:18:38 -0400

    First layer, get the links to all the events

--- uoftscrapers/scrapers/events/__init__.py
+++ uoftscrapers/scrapers/events/__init__.py
@@ -6,6 +6,10 @@ import requests
 import pytz
 import json
 
+
+import urllib.parse as urlparse
+from urllib.parse import urlencode
+
 class UTEvents:
     """A scraper for Events at the University of Toronto."""
 
@@ -22,7 +26,29 @@ class UTEvents:
 
     @staticmethod
     def scrape(location='.'):
+
         Scraper.logger.info('UTEvents initialized.')
-        Scraper.logger.info('Not implemented.')
-        raise NotImplementedError('This scraper has not been implemented yet.')
+        return UTEvents.get_events_links()
         Scraper.logger.info('UTEvents completed.')
+
+    @staticmethod
+    def get_events_links():
+        page_index_url = UTEvents.host + "index.php"
+        url_parts = list(urlparse.urlparse(page_index_url))
+        events_links = []
+        paging_index = 1
+        events_count = 10 
+        while(events_count == 10):
+            params = {
+                    'p': paging_index
+                }
+            url_parts[4] = urlencode(params)
+            paging_index += 1
+            html = UTEvents.s.get(urlparse.urlunparse(url_parts)).text
+            soup = BeautifulSoup(html, 'html.parser')
+            events_dom_arr = soup.select('#results')[0].find_all('li')
+            events_count = len(events_dom_arr)
+            events_links += list(map(lambda e: e.a['href'], events_dom_arr))
+        return(events_links)                
+
+
