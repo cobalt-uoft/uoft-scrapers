2f1bc4feae08756d46a0b9068a3c174269ac7ee9
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-04-25 09:19:06 -0400

    Recent update from the library normalized the urls

--- uoftscrapers/scrapers/libraries/__init__.py
+++ uoftscrapers/scrapers/libraries/__init__.py
@@ -11,33 +11,28 @@ class Libraries:
     host = 'https://onesearch.library.utoronto.ca/visit'
 
     campuses_tags = {
-    	'St. George': 'UTSG', 
-    	'U of T Mississauga': 'UTM', 
-    	'U of T Scarborough': 'UTSC'
-    	}
+        'St. George': 'UTSG', 
+        'U of T Mississauga': 'UTM', 
+        'U of T Scarborough': 'UTSC'
+        }
 
     @staticmethod
     def scrape(location='.'):
-    	Scraper.logger.info('Libraries initialized.')
-    	Scraper.ensure_location(location)
-    	return Libraries.get_library_link()
-    	# ['content_link'] -> 'Teaser Text'
-    	# ['info_link'] -> 'Everything Else'
-    	library_data_links = Libraries.get_library_link()
-    	raise NotImplementedError('This scraper has not been implemented yet.')
-    	Scraper.logger.info('Libraries completed.')
+        Scraper.logger.info('Libraries initialized.')
+        Scraper.ensure_location(location)
+        library_data_links = Libraries.get_library_link()
+        raise NotImplementedError('This scraper has not been implemented yet.')
+        Scraper.logger.info('Libraries completed.')
 
     @staticmethod
     def get_library_link():
-    	html = Scraper.get(Libraries.host)
-    	soup = BeautifulSoup(html, 'html.parser')
-    	list_obj_arr = soup.select('.view-list-of-libraries')[1].select(
-			'.view-content')[0].select('.views-row')
-    	library_links = dict()
-    	for l in list_obj_arr:
-    		title = l.h2.text
-    		library_links[title] = {
-    			'content_link': l.select('a')[0]['href'],
-    			'info_link': l.select('a')[1]['href']
-    		}
-    	return library_links
+        html = Scraper.get(Libraries.host)
+        soup = BeautifulSoup(html, 'html.parser')
+        list_obj_arr = soup.select('.view-list-of-libraries')[1].select(
+            '.view-content')[0].select('.views-row')
+        library_links = [l.a['href'] for l in list_obj_arr]
+        return library_links
+
+    @staticmethod
+    def get_library_doc(url_tail):
+        pass
