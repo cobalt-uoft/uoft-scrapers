0da4f0f34c77cb61595eb34f642575a117d9e05e
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-04-25 14:12:25 -0400

    Edge cases and malformed urls

--- uoftscrapers/scrapers/libraries/__init__.py
+++ uoftscrapers/scrapers/libraries/__init__.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup, NavigableString, Comment
 from datetime import datetime, date
 from collections import OrderedDict
 
+
 class Libraries:
     """A scraper for the Libraries at the University of Toronto."""
     host = 'https://onesearch.library.utoronto.ca/'
@@ -11,11 +12,14 @@ class Libraries:
     def scrape(location='.'):
         Scraper.logger.info('Libraries initialized.')
         Scraper.ensure_location(location)
-        return Libraries.get_library_doc(Libraries.get_library_link()[0])
 
         for library_link in Libraries.get_library_link():
             doc = Libraries.get_library_doc(library_link)
-            #Scraper.save_json(doc, location, library_link.split('/')[-1])
+            if (doc is not None):
+                Scraper.save_json(doc, location, library_link.split('/')[-1])
+            else:
+                # Not a real library page
+                Scraper.logger.info('Skipped: ' + library_link.split('/')[-1])
         Scraper.logger.info('Libraries completed.')
 
     @staticmethod
@@ -55,14 +59,18 @@ class Libraries:
         html = Scraper.get(calendar_link, max_attempts=5)
         week = []
         if html is None:
-            week = [''] * 7
+            week = ['Hours are not available, please contact the library.'] * 7
         else:
             soup = BeautifulSoup(html, 'html.parser')
-            week = soup.select('.calendar-wrapper')[0].select(
-                '.start-week')[1].select('.start-day')
+            if soup.select('.calendar-wrapper') != []:
+                week = soup.select('.calendar-wrapper')[0].select(
+                    '.start-week')[1].select('.start-day')
+                week = list(map(Libraries.normalize_text_sections, week))
+            else:
+                week = ['Hours are not available, login required.'] * 7
         hours = OrderedDict()
         for day in range(len(weekdays)):
-            hour = Libraries.normalize_text_sections(week[day])
+            hour = week[day]
             hours[weekdays[day]] = hour
         return hours
 
@@ -71,14 +79,21 @@ class Libraries:
         library_url = Libraries.host + url_tail
         html = Scraper.get(library_url)
         soup = BeautifulSoup(html, 'html.parser')
-        main_content = soup.select('#content-inner')[0].select('.library-info')[0]
+        main_content = soup.select('#content-inner')[0].select('.library-info')
+
+        if main_content == []:
+            return None
+        else:
+            main_content = main_content[0]
 
         library_name = main_content.h1.extract().text
-        library_image = main_content.img.extract()['src']
-        
+        library_image = ''
+        if main_content.img is not None:
+            library_image = main_content.img.extract()['src']
+
         for content in main_content.select('.field-content'):
             content.extract()
-        
+
         library_website = main_content.a.extract()['href']
 
         library_hours_link = main_content.a['href']
@@ -86,16 +101,26 @@ class Libraries:
         for day, hour in library_hours.items():
             library_hours[day] = hour[1:].strip()
 
-        library_address = Libraries.normalize_text_sections(
-            main_content.select('.library-address')[0].extract())
+        library_address = ''
+        if main_content.select('.library-address') != []:
+            library_address = Libraries.normalize_text_sections(
+                main_content.select('.library-address')[0].extract())
 
-        library_phone = '' 
+        library_phone = ''
         if main_content.select('.phone') != []:
-            library_phone = main_content.select('.phone')[0].extract().text.strip()
+            library_phone = main_content.select(
+                '.phone')[0].extract().text.strip()
 
         library_info = main_content.select('.library-info-text')[0].extract()
-        library_info_titles = [s.text for s in  library_info.select('h2')]
-        library_info_texts = library_info.select('p') + library_info.select('ul')
+        library_info_titles = [s.text for s in library_info.select('h2')]
+        library_info_texts = []
+
+        for content in library_info.contents:
+            if type(content) != NavigableString:
+                if content.name in ['p', 'ul', 'ol']:
+                    library_info_texts.append(content)
+        library_info_texts = list(
+            map(Libraries.normalize_text_sections, library_info_texts))
 
         library_about = ''
         library_collection_strenghts = ''
@@ -103,18 +128,16 @@ class Libraries:
 
         for i in range(len(library_info_titles)):
             if library_info_titles[i] == 'About the library':
-                library_about = Libraries.normalize_text_sections(
-                    library_info_texts[i])
+                library_about = library_info_texts[i]
             elif library_info_titles[i] == 'Collection strengths':
-                library_collection_strenghts = Libraries.normalize_text_sections(
-                    library_info_texts[i]).replace('  ', ', ')
+                library_collection_strenghts = library_info_texts[i].replace(
+                    '  ', ', ')
             elif library_info_titles[i] == 'How to access':
-                library_how_to_access = Libraries.normalize_text_sections(
-                    library_info_texts[i])
+                library_how_to_access = library_info_texts[i]
 
         doc = OrderedDict([
             ('name', library_name),
-            ('image', library_image), 
+            ('image', library_image),
             ('website', library_website),
             ('hours', library_hours),
             ('address', library_address),
