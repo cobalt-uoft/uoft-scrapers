0b017b980fe01286a4973351e7e61ece95cc2dd9
Anderson Ng Ho Yin <anderson202@live.hk>
2016-05-12 17:41:41 -0400

    Produced a functional scraper
    the scraper and now functional, but the JSON file names may have to be
    changed

--- uoftscrapers/scrapers/calendar/utm.py
+++ uoftscrapers/scrapers/calendar/utm.py
@@ -12,31 +12,53 @@ class UTMCalendar:
 
     host1 = 'http://m.utm.utoronto.ca/importantDates.php?mode=full&session={0}5&header='
     host2 = 'http://m.utm.utoronto.ca/importantDates.php?mode=full&session={0}9&header='
+    sessionLinks = [host1, host2]
 
     @staticmethod
     def scrape(location='.', year=None):
         year = year or now.year
 
+        calendar = OrderedDict()
         Scraper.logger.info('UTMCalendar initialized.')
+        for link in UTMCalendar.sessionLinks:
+            html = Scraper.get(link.format(year))
+            soup = BeautifulSoup(html, 'html.parser')
+            content = soup.find('div', class_='content')
+            dates = content.find_all('div', class_='title')
+            i = 0
+            currentDate = dates[i]
+            while(i<len(dates)):
+                date = dates[i].text
+                events = []
+                while (currentDate == dates[i]):
+                    info = dates[i].find_next('div', class_='info')
+                    description = info.text
+                    eventStartEnd = date.split('-')
+                    eventStart = eventStartEnd[0].strip()
+                    if len(eventStartEnd)>1:
+                        eventEnd = eventStartEnd[1].strip()
+                    else:
+                        eventEnd = eventStart
 
-        html = Scraper.get(UTMCalendar.host1.format(year))
-        soup = BeautifulSoup(html, 'html.parser')
-        content = soup.find('div', class_='content')
-        dates = content.find_all('div', class_='title')
-        i = 0
-        currentDate = dates[i]
-        while(i<len(dates)):
-            print(dates[i].text)
-
-            while (currentDate == dates[i]):
-                info = dates[i].find_next('div', class_='info')
-                print(info.text)
-                i+=1
-                if(i>=len(dates)):
-                    break;
-            if(i<len(dates)):
-                currentDate = dates[i]
+                    events.append(OrderedDict([
+                            ('end_date', eventEnd),
+                            ('campus', 'UTM'),
+                            ('description', description)
+                        ]))
+                    i+=1
+                    if(i>=len(dates)):
+                        break;
+                calendar[date] = OrderedDict([
+                        ('date', eventStart),
+                        ('session', "Summer"),
+                        ('events', events)
+                    ])
+                if(i<len(dates)):
+                    currentDate = dates[i]
 
 
+        for date, info in calendar.items():
+            Scraper.save_json(info, location, date)
 
         Scraper.logger.info('UTMCalendar completed.')
+        return calendar
